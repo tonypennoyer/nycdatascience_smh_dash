459840d85a966c201a6f4741ec61e54cfd082264
Daniel Nie <danielx.nie@gmail.com>
2021-12-15 14:14:00 -0500

    added app.layout object

--- app.py
+++ app.py
@@ -8,3 +8,9 @@ app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheet
                 meta_tags=[{'name': 'viewport',
                             'content': 'width=device-width, initial-scale=1.0'}]
                 )
+
+app.layout = dash.html.Div(children=[
+    dash.html.H1(children='Assets')])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
